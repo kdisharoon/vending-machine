71eaf0c16ec4dd72e46365c38ff37cef82457c3b
Kristofer Disharoon <kdisharoon@gmail.com>
2021-06-03 12:08:43 -0400

    merging

--- src/main/java/com/techelevator/Chip.java
+++ src/main/java/com/techelevator/Chip.java
@@ -1,10 +1,12 @@
 package com.techelevator;
 
+import java.math.BigDecimal;
+
 public class Chip extends Product {
 
     private String chipSlogan = "Crunch Crunch";
 
-    public Chip(String productName, double productPrice) {
+    public Chip(String productName, BigDecimal productPrice) {
         super(productName, productPrice);
 
     }
