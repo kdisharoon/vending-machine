39e367228ace3b741fbd727d36ce026e3a6c67aa
Michael Buckner <bucknerml@gmail.com>
2021-06-03 14:56:19 -0400

    Added basic Menu and VendingMachine classes.

---
+++ src/main/java/com/techelevator/Menu.java
@@ -0,0 +1,25 @@
+package com.techelevator;
+
+import java.util.List;
+
+public class Menu {
+    // Instantiate a vending machine
+    // Present a series of options
+    // According to the
+    private List<String> intialOptions;
+
+    public Menu() {
+
+    }
+
+    public void displayInitialMenu() {
+
+    }
+
+    public static void main(String[] args) {
+        VendingMachine vm = new VendingMachine();
+        Menu
+    }
+
+
+}
---
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -0,0 +1,14 @@
+package com.techelevator;
+
+import java.math.BigDecimal;
+import java.util.Set;
+import java.util.HashSet;
+
+public class VendingMachine {
+    private BigDecimal currentMoneyInMachine;
+    private Set<Slot> listOfSlots = new HashSet<>();
+
+    // Stock the machine
+
+
+}
