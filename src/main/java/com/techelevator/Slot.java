3178501a711262b0bde78b6f79bba9610cc4ae61
Kristofer Disharoon <kdisharoon@gmail.com>
2021-06-03 12:29:29 -0400

    Did initial getters/setters of Slot class

--- src/main/java/com/techelevator/Slot.java
+++ src/main/java/com/techelevator/Slot.java
@@ -2,8 +2,31 @@ package com.techelevator;
 
 public class Slot {
 
-    // product
-    private int quantity;
+    private String slotName;        //A7, B3, etc.
+    private Product product;        //product in this particular slot
+    private int quantity;           //quantity on hand
 
+    public Slot(String slotName) {
+        this.slotName = slotName;
+    }
 
+    public String getSlotName() {
+        return slotName;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
