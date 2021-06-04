f4caa40c572ac414174e2d870aba7c30251123c7
Michael Buckner <bucknerml@gmail.com>
2021-06-04 17:04:35 -0400

    Major refactoring
    Moved methods getChange, dispense, and feedMoney from Menu.java to
    VendingMachine.java.

--- src/main/java/com/techelevator/Menu.java
+++ src/main/java/com/techelevator/Menu.java
@@ -2,29 +2,23 @@ package com.techelevator;
 
 import java.io.*;
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.text.NumberFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Arrays;
-import java.util.InputMismatchException;
 
-public class Menu <T> {
-    VendingMachine vm = new VendingMachine();
-    Scanner keyboard = new Scanner(System.in);
-    List<String> initialMenu = vm.getInitialMenuOptions();
-    List<String> purchaseMenu = vm.getPurchaseFlowMenu();
-    List<Slot> inventory = vm.getListOfSlots();
-    NumberFormat nf = NumberFormat.getCurrencyInstance();
-    List<BigDecimal> validAmounts = Arrays.asList(new BigDecimal("1.00"), new BigDecimal("2.00"), new BigDecimal("5.00"),
-            new BigDecimal("10.00"), new BigDecimal("20.00"), new BigDecimal("50.00"),
-            new BigDecimal("100.00"));
+public class Menu {
+    private final VendingMachine VM = new VendingMachine();
+    private final Scanner keyboard = new Scanner(System.in);
+    private final List<String> initialMenu = VM.getInitialMenu();
+    private final List<String> purchaseMenu = VM.getPurchaseMenu();
+    private final List<Slot> inventory = VM.getListOfSlots();
+    private final NumberFormat nf = NumberFormat.getCurrencyInstance();
 
     public <T> String getUserChoice(List<T> relevantMenu) {
-        String userChoice = "";
+        String userChoice;
 
         userChoice = keyboard.nextLine();
 
@@ -37,8 +31,6 @@ public class Menu <T> {
         return userChoice;
     }
 
-
-
     public <T> void printMenu(List<T> listToPrint) {
         for (T t : listToPrint) {
             System.out.println(t);
@@ -46,67 +38,29 @@ public class Menu <T> {
         System.out.println();
     }
 
-    public String feedMoney() {
-        BigDecimal amount;
-        String message = "FEED MONEY:";
-
-        System.out.println("Input amount of money:");
-
-        try {
-            amount = keyboard.nextBigDecimal();
-        }
-        catch (InputMismatchException e) {
-            amount = BigDecimal.ZERO;
-        }
-
-        keyboard.nextLine();
-        BigDecimal roundedAmount = amount.setScale(2, RoundingMode.FLOOR);
-
-        if (validAmounts.contains(roundedAmount)) {
-            vm.addMoney(roundedAmount);
-            System.out.println(nf.format(roundedAmount) + " added to your balance.\n\n");
-            return message + " " + nf.format(amount) + " " + nf.format(vm.getCurrentMoneyInMachine());
-        } else {
-            System.out.println("Invalid amount.");
-            return null;
-        }
-
-    }
-
-    public void dispense(Slot s) {
-        Product currentProduct = s.getProduct();
-        String itemName = currentProduct.getName();
-        BigDecimal itemPrice = currentProduct.getPrice();
-        vm.subtractMoney(itemPrice);
-        BigDecimal currentMoney = vm.getCurrentMoneyInMachine();
-        String itemSaleMessage = currentProduct.getSaleMessage();
-
-        s.sellItem();       //reduces quantity in this slot by 1
-
-        System.out.println(itemName + " costs " + nf.format(itemPrice) + ".\nYou have " + nf.format(currentMoney)
-                            + " remaining in the machine." + "\n" + itemSaleMessage + "\n\n");
-    }
-
     public String selectProduct() {
         System.out.println("Here are the currently available products: \n");
         printMenu(inventory);
         System.out.println("Please enter the slot of the product you'd like to purchase (i.e. A2, B4): ");
         String userInput = keyboard.nextLine().toUpperCase();
-        String message = null;
+        String productLogMessage = null;
 
         boolean found = false;
         for (Slot s : inventory) {
             if (s.getSlotID().equals(userInput)) {
                 BigDecimal price = s.getProduct().getPrice();
-                BigDecimal currentMoney = vm.getCurrentMoneyInMachine();
+                BigDecimal currentMoney = VM.getCurrentMoneyInMachine();
                 found = true;
 
                 if (s.getQuantity() > 0) {
                     if (price.compareTo(currentMoney) <= 0) {
-                        BigDecimal balanceBeforePurchase = vm.getCurrentMoneyInMachine();
-                        dispense(s);
-                        message = s.getProduct().getName() + " " + s.getSlotID() + " "
-                                + nf.format(balanceBeforePurchase) + " " + nf.format(vm.getCurrentMoneyInMachine());
+                        BigDecimal balanceBeforePurchase = VM.getCurrentMoneyInMachine();
+                        String dispenseMsg = VM.dispense(s);
+
+                        System.out.println(dispenseMsg);
+
+                        productLogMessage = s.getProduct().getName() + " " + s.getSlotID() + " "
+                                + nf.format(balanceBeforePurchase) + " " + nf.format(VM.getCurrentMoneyInMachine());
                     } else {
                         System.out.println("You don't have enough money for that product.");
                     }
@@ -123,7 +77,7 @@ public class Menu <T> {
             System.out.println("Sorry, that's not a valid slot.\n");
         }
 
-        return message;
+        return productLogMessage;
     }
 
     /**
@@ -131,7 +85,7 @@ public class Menu <T> {
      */
     public void processPurchaseChoice() {
 
-        String purchaseChoice = "";
+        String purchaseChoice;
         String logFileName = "Log.txt";
 
         try (FileWriter fw = new FileWriter(logFileName, true);
@@ -140,16 +94,16 @@ public class Menu <T> {
             do {
                 System.out.println("\nWhat would you like to do?");
                 printMenu(purchaseMenu);
-                System.out.println("Current Money Provided: " + nf.format(vm.getCurrentMoneyInMachine()));
+                System.out.println("Current Money Provided: " + nf.format(VM.getCurrentMoneyInMachine()));
                 purchaseChoice = getUserChoice(purchaseMenu);
                 String msgForLog;
 
                 if (purchaseChoice.equals("1")) {
-                    msgForLog = feedMoney();
+                    msgForLog = VM.feedMoney();
                 } else if (purchaseChoice.equals("2")) {
                     msgForLog = selectProduct();
                 } else {
-                    msgForLog = getChange();
+                    msgForLog = VM.getChange();
                 }
 
                 String currentDateTime = getCurrentDateAndTime();
@@ -158,43 +112,12 @@ public class Menu <T> {
                     msgForLog = currentDateTime + " " + msgForLog;
                     auditLogWriter.println(msgForLog);
                 }
-
-    //            auditLogWriter.flush();
-
             } while ((purchaseChoice.equals("1")) || (purchaseChoice.equals("2")));
 
         } catch (IOException e) {
             System.out.println("Unable to find or create " + logFileName + ".");
         }
 
-    }
-
-    public String getChange() {
-        BigDecimal initialBalance = vm.getCurrentMoneyInMachine();
-
-        if (initialBalance.equals(BigDecimal.ZERO)) return null;
-
-        BigDecimal quarter = new BigDecimal("0.25");
-        BigDecimal dime = new BigDecimal("0.10");
-        BigDecimal nickel = new BigDecimal("0.05");
-        int numberOfQuarters;
-        int numberOfDimes;
-        int numberOfNickels;
-        String message = "GIVE CHANGE:";
-
-        numberOfQuarters = vm.getCurrentMoneyInMachine().divide(quarter).intValue();
-        vm.subtractMoney(quarter.multiply(new BigDecimal(numberOfQuarters)));
-
-        numberOfDimes = vm.getCurrentMoneyInMachine().divide(dime).intValue();
-        vm.subtractMoney(dime.multiply(new BigDecimal(numberOfDimes)));
-
-        numberOfNickels = vm.getCurrentMoneyInMachine().divide(nickel).intValue();
-        vm.subtractMoney(nickel.multiply(new BigDecimal(numberOfNickels)));
-
-        System.out.println("Your change is " + numberOfQuarters + " quarters, "
-                            + numberOfDimes + " dimes, " + numberOfNickels + " nickels");
-
-        return message + " " + nf.format(initialBalance) + " " + nf.format(vm.getCurrentMoneyInMachine());
     }
 
     public String getCurrentDateAndTime() {
@@ -213,7 +136,7 @@ public class Menu <T> {
         do {
             System.out.println("\nWhat would you like to do?");
             printMenu(initialMenu);
-            userChoice = getUserChoice(vm.getInitialMenuOptions());
+            userChoice = getUserChoice(VM.getInitialMenu());
 
             if (userChoice.equals("1")) {
                 printMenu(inventory);
@@ -231,6 +154,4 @@ public class Menu <T> {
         vendingMachineMenu.run();
 
     }
-
-
 }
--- src/main/java/com/techelevator/VendingMachine.java
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -3,6 +3,8 @@ package com.techelevator;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
 import java.util.*;
 
 public class VendingMachine {
@@ -10,11 +12,17 @@ public class VendingMachine {
     private final String OPTION2 = "(2) Purchase";
     private final String OPTION3 = "(3) Exit";
     private final String[] initialMenuOptions = {OPTION1, OPTION2, OPTION3};
-    private final String[] purchaseFlowMenuOptions = {"(1) Feed Money", "(2) Select Product", "(3) Finish Transaction"};
+    private final String[] purchaseMenuOptions = {"(1) Feed Money", "(2) Select Product", "(3) Finish Transaction"};
     private BigDecimal currentMoneyInMachine = BigDecimal.ZERO;
-    private List<Slot> listOfSlots = new ArrayList<>();
-    private List<String> menuOptions = new ArrayList<>(Arrays.asList(initialMenuOptions));
-    private List<String> purchaseFlowMenu = new ArrayList<>(Arrays.asList(purchaseFlowMenuOptions));
+    private final List<Slot> listOfSlots = new ArrayList<>();
+    private final List<String> INITIAL_MENU = new ArrayList<>(Arrays.asList(initialMenuOptions));
+    private final List<String> purchaseMenu = new ArrayList<>(Arrays.asList(purchaseMenuOptions));
+    private final NumberFormat nf = NumberFormat.getCurrencyInstance();
+    private final Scanner keyboard = new Scanner(System.in);
+    private final List<BigDecimal> validAmounts = Arrays.asList(new BigDecimal("1.00"), new BigDecimal("2.00"), new BigDecimal("5.00"),
+            new BigDecimal("10.00"), new BigDecimal("20.00"), new BigDecimal("50.00"),
+            new BigDecimal("100.00"));
+
 
     public VendingMachine() {
         stock();
@@ -24,12 +32,12 @@ public class VendingMachine {
         return listOfSlots;
     }
 
-    public List<String> getInitialMenuOptions() {
-        return menuOptions;
+    public List<String> getInitialMenu() {
+        return INITIAL_MENU;
     }
 
-    public List<String> getPurchaseFlowMenu() {
-        return purchaseFlowMenu;
+    public List<String> getPurchaseMenu() {
+        return purchaseMenu;
     }
 
     // Stock the machine
@@ -69,15 +77,6 @@ public class VendingMachine {
         }
     }
 
-
-    public void purchase(Slot slot) {
-
-    }
-
-    public void dispense(Product product) {
-
-    }
-
     public void addMoney(BigDecimal amountToAdd) {
         currentMoneyInMachine = currentMoneyInMachine.add(amountToAdd);
     }
@@ -86,7 +85,81 @@ public class VendingMachine {
         currentMoneyInMachine = currentMoneyInMachine.subtract(amountToSubtract);
     }
 
+    public String feedMoney() {
+        BigDecimal amount;
+        String message = "FEED MONEY:";
+
+        System.out.println("Input amount of money:");
+
+        try {
+            amount = keyboard.nextBigDecimal();
+        }
+        catch (InputMismatchException e) {
+            amount = BigDecimal.ZERO;
+        }
+
+        keyboard.nextLine();
+        BigDecimal roundedAmount = amount.setScale(2, RoundingMode.FLOOR);
+
+        if (validAmounts.contains(roundedAmount)) {
+            addMoney(roundedAmount);
+            System.out.println(nf.format(roundedAmount) + " added to your balance.\n\n");
+            return message + " " + nf.format(amount) + " " + nf.format(getCurrentMoneyInMachine());
+        } else {
+            System.out.println("Invalid amount.");
+            return null;
+        }
+
+    }
+
+    public String dispense(Slot s) {
+        Product currentProduct = s.getProduct();
+        String itemName = currentProduct.getName();
+        BigDecimal itemPrice = currentProduct.getPrice();
+        subtractMoney(itemPrice);
+        BigDecimal currentMoney = getCurrentMoneyInMachine();
+        String itemSaleMessage = currentProduct.getSaleMessage();
+
+        s.sellItem();       //reduces quantity in this slot by 1
+
+        return itemName + " costs " + nf.format(itemPrice) + ".\nYou have " + nf.format(currentMoney)
+                + " remaining in the machine." + "\n" + itemSaleMessage + "\n\n";
+    }
+
+
+    public String getChange() {
+        BigDecimal initialBalance = getCurrentMoneyInMachine();
+
+        if (initialBalance.equals(BigDecimal.ZERO)) return null;
+
+        BigDecimal quarter = new BigDecimal("0.25");
+        BigDecimal dime = new BigDecimal("0.10");
+        BigDecimal nickel = new BigDecimal("0.05");
+        int numberOfQuarters;
+        int numberOfDimes;
+        int numberOfNickels;
+        String message = "GIVE CHANGE:";
+
+        numberOfQuarters = getCurrentMoneyInMachine().divide(quarter).intValue();
+        subtractMoney(quarter.multiply(new BigDecimal(numberOfQuarters)));
+
+        numberOfDimes = getCurrentMoneyInMachine().divide(dime).intValue();
+        subtractMoney(dime.multiply(new BigDecimal(numberOfDimes)));
+
+        numberOfNickels = getCurrentMoneyInMachine().divide(nickel).intValue();
+        subtractMoney(nickel.multiply(new BigDecimal(numberOfNickels)));
+
+        System.out.println("Your change is " + numberOfQuarters + " quarters, "
+                + numberOfDimes + " dimes, " + numberOfNickels + " nickels");
+
+        return message + " " + nf.format(initialBalance) + " " + nf.format(getCurrentMoneyInMachine());
+    }
+
     public BigDecimal getCurrentMoneyInMachine() {
         return currentMoneyInMachine;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
